51d8826114155bdd14e261b7a59e67b96c98f669
Ernest Morse <enmorse48@gmail.com>
2021-10-29 12:33:06 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# Make a list of your favorite hobbies and give
+# the list the variable name games. Now make a
+# list of your favorite foods and name the variable
+# foods. Join the two lists and name the result
+# favorites. Finally, print the variable favorites.
+
+games = [
+    'baseball',
+    'bodybuilding,' 
+    'vampire the masquerade by night'
+    'football',
+    'diablo 2'
+    'd2r'
+    'd&d',
+    'magic the gathering'
+    'running',
+    'sca heavy combat'
+    'vampire the masquerade',
+    'weight lifting',
+]
+
+foods = [
+    'beef',
+    'cereal',
+    'chicken',
+    'fruit',
+    'vegetables',
+]
+
+favorites = games + foods
+
+print(favorites)
